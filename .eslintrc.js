ec0e39134451a563c1e00dea4cd5c14c3f44b1f5
daseuls <dls2tmfs2@naver.com>
2022-07-15 09:41:05 +0900

    feat: initial setting

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/no-shadow': ['error'],
+        'no-shadow': 'off',
+        'no-undef': 'off',
+        'no-empty-function': 'off',
+        '@typescript-eslint/no-empty-function': 'off',
+        'react/display-name': 'off',
+        'react/prop-types': 'off',
+      },
+    },
+  ],
+};
