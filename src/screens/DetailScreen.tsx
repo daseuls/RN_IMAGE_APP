04379753ccaadcc90c9b8b9aa308e1f2ace72b0a
daseuls <dls2tmfs2@naver.com>
2022-07-16 16:42:19 +0900

    feat: detail screen 레이아웃

--- src/screens/DetailScreen.tsx
+++ src/screens/DetailScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, TextInput, View, ScrollView} from 'react-native';
 import {RootStackParamList} from '../types/navigator';
 import {StackScreenProps} from '@react-navigation/stack';
 import FastImage from 'react-native-fast-image';
@@ -8,19 +8,34 @@ import {windowWidth} from '../utils/dimensions';
 type IProps = StackScreenProps<RootStackParamList, 'Detail'>;
 
 const DetailScreen = ({route}: IProps) => {
-  const {user} = route.params.data;
+  const {user, alt_description, description, urls} = route.params.data;
 
   console.log(route.params.data);
   return (
     <View style={styles.container}>
-      <View style={styles.userInfoContainer}>
-        <FastImage
-          style={styles.userImage}
-          source={{uri: user.profile_image.small}}
-        />
-        <Text style={styles.userName}>{user.name}</Text>
-        <Text style={styles.userRole}> · Instructor</Text>
-      </View>
+      <ScrollView>
+        <View style={styles.userInfoContainer}>
+          <FastImage
+            style={styles.userImage}
+            source={{uri: user.profile_image.small}}
+          />
+          <Text style={styles.userName}>{user.name}</Text>
+          <Text style={styles.userRole}> · Instructor</Text>
+        </View>
+        <View>
+          <Text>{alt_description}</Text>
+          <Text>{description}</Text>
+          <View style={styles.imageContainer}>
+            <FastImage style={styles.photoImage} source={{uri: urls.small}} />
+          </View>
+        </View>
+        <View style={styles.commentsContainer} />
+      </ScrollView>
+      <TextInput
+        style={styles.input}
+        placeholder="생각을 나눠보세요."
+        keyboardType="numeric"
+      />
     </View>
   );
 };
@@ -54,5 +69,27 @@ const styles = StyleSheet.create({
     fontSize: windowWidth * 0.04,
     fontWeight: '700',
     color: '#7D88A8',
+  },
+
+  photoImage: {
+    width: windowWidth * 0.9,
+    height: windowWidth * 0.9,
+    borderRadius: 15,
+  },
+
+  contentsContainer: {},
+
+  imageContainer: {
+    alignItems: 'center',
+  },
+
+  commentsContainer: {},
+
+  input: {
+    borderColor: '#BCC4D8',
+    borderRadius: 15,
+    borderWidth: 2,
+    padding: 10,
+    marginBottom: 20,
   },
 });
