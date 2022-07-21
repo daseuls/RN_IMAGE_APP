5359e4f7f7175c70493cf6258ec177437badd1fb
daseuls <dls2tmfs2@naver.com>
2022-07-22 01:14:38 +0900

    feat: comments 없을때 화면 조건부 렌더링

---
+++ src/components/CommentBar.tsx
@@ -0,0 +1,225 @@
+import React, {useState} from 'react';
+import {
+  Text,
+  View,
+  TouchableNativeFeedback,
+  StyleSheet,
+  TextInput,
+} from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import {useDispatch, useSelector} from 'react-redux';
+import AsyncStorage from '@react-native-community/async-storage';
+import {IImageItem, IRootState} from '../types';
+import {imageInfoSlice} from '../store';
+
+const CommentBar = ({
+  data,
+  navigation,
+  isContentsShowing,
+  isShowingBtn,
+  flatListRef,
+}) => {
+  const {description, id, comments, isBookmarked} = data;
+
+  const imageList = useSelector((state: IRootState) => {
+    return state.imageInfo.value;
+  });
+  const dispatch = useDispatch();
+
+  const [commentValue, setCommentValue] = useState('');
+
+  const onPressSubmitCommentBtn = () => {
+    if (commentValue) {
+      setCommentValue('');
+
+      const commentData = {
+        id: new Date().valueOf(),
+        text: commentValue,
+        isLiked: false,
+      };
+
+      if (comments) {
+        const updatedList = imageList.map((imageInfo: IImageItem) =>
+          imageInfo.id === id
+            ? {
+                ...imageInfo,
+                comments: [...imageInfo.comments, commentData],
+              }
+            : imageInfo,
+        );
+        navigation.setParams({
+          data: {
+            ...data,
+            comments: [...comments, commentData],
+          },
+        });
+        dispatch(imageInfoSlice.actions.update(updatedList));
+        AsyncStorage.setItem('imageList', JSON.stringify(updatedList));
+      } else {
+        const updatedList = imageList.map((imageInfo: IImageItem) =>
+          imageInfo.id === id
+            ? {...imageInfo, comments: [commentData]}
+            : imageInfo,
+        );
+        navigation.setParams({
+          data: {
+            ...data,
+            comments: [commentData],
+          },
+        });
+        dispatch(imageInfoSlice.actions.update(updatedList));
+        AsyncStorage.setItem('imageList', JSON.stringify(updatedList));
+      }
+    }
+  };
+
+  const onPressBookmarkBtn = (imageId: string, bool: boolean) => {
+    const updatedList = imageList.map(el =>
+      el.id === imageId ? {...el, isBookmarked: bool} : el,
+    );
+    navigation.setParams({
+      data: {
+        ...data,
+        isBookmarked: bool,
+      },
+    });
+    dispatch(imageInfoSlice.actions.update(updatedList));
+    AsyncStorage.setItem('imageList', JSON.stringify(updatedList));
+  };
+
+  return (
+    <>
+      {!isContentsShowing && (
+        <View style={styles.previewContentsContainer}>
+          <TouchableNativeFeedback
+            onPress={() => flatListRef.current?.scrollToOffset({offset: 5000})}>
+            <Text>{description}</Text>
+          </TouchableNativeFeedback>
+          <Ionicons name="chevron-up" size={20} />
+        </View>
+      )}
+      {isShowingBtn && (
+        <TouchableNativeFeedback
+          onPress={() => flatListRef.current?.scrollToEnd()}>
+          <View style={styles.moveCurrentCommentBtn}>
+            <Text style={styles.btnText}>최신 댓글로</Text>
+          </View>
+        </TouchableNativeFeedback>
+      )}
+      <View style={styles.likedIconContainer}>
+        <Ionicons
+          onPress={() => onPressBookmarkBtn(id, !isBookmarked)}
+          name="heart-sharp"
+          size={30}
+          color={isBookmarked ? '#FF7272' : '#C4C8D3'}
+          style={styles.likeBtn}
+        />
+        <Text
+          style={{
+            ...styles.likedText,
+            color: isBookmarked ? '#585A5F' : '#B9BDC7',
+          }}>
+          {isBookmarked
+            ? '좋아한 게시글이에요 !'
+            : '게시글이 맘에 들면 좋아요를 눌러보세요!'}
+        </Text>
+      </View>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="생각을 나눠보세요."
+          keyboardType="numeric"
+          onChangeText={text => setCommentValue(text)}
+          clearTextOnFocus
+          value={commentValue}
+        />
+        <Ionicons
+          onPress={onPressSubmitCommentBtn}
+          name="send"
+          style={styles.sendIcon}
+        />
+      </View>
+    </>
+  );
+};
+
+export default CommentBar;
+
+const styles = StyleSheet.create({
+  likedIconContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderTopColor: '#BCC4D8',
+    borderTopWidth: 1,
+    marginHorizontal: 10,
+    width: '100%',
+  },
+
+  likedText: {
+    fontWeight: '700',
+  },
+
+  likeBtn: {
+    marginRight: 5,
+  },
+
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 20,
+    marginHorizontal: 10,
+  },
+
+  input: {
+    borderColor: '#BCC4D8',
+    borderRadius: 15,
+    borderWidth: 1.5,
+    padding: 12,
+    width: '100%',
+  },
+
+  sendIcon: {
+    color: '#BCC4D8',
+    fontSize: 20,
+    position: 'absolute',
+    right: 10,
+  },
+
+  previewContentsContainer: {
+    borderBottomWidth: 0.5,
+    borderColor: '#BCC4D8',
+    paddingHorizontal: 30,
+    paddingVertical: 20,
+    position: 'absolute',
+    top: 0,
+    width: '100%',
+    zIndex: 100,
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+
+  moveCurrentCommentBtn: {
+    position: 'absolute',
+    bottom: 135,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 20,
+    backgroundColor: 'white',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.18,
+    shadowRadius: 1.0,
+    elevation: 1,
+  },
+
+  btnText: {
+    fontWeight: '900',
+    color: '#FF7272',
+  },
+});
--- src/screens/DetailScreen.tsx
+++ src/screens/DetailScreen.tsx
@@ -1,31 +1,25 @@
 import React, {useRef, useState} from 'react';
-import {
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-  FlatList,
-  TouchableNativeFeedback,
-} from 'react-native';
+import {StyleSheet, FlatList, Text, View} from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
 import {StackScreenProps} from '@react-navigation/stack';
 import AsyncStorage from '@react-native-community/async-storage';
 import FastImage from 'react-native-fast-image';
 import CommentItem from '../components/CommentItem';
 import {windowWidth} from '../utils/dimensions';
-import {IImageItem, IRootState} from '../types/index';
+import {IRootState} from '../types/index';
 import {RootStackParamList} from '../types/navigator';
 import {imageInfoSlice} from '../store';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import {TouchableOpacity} from 'react-native-gesture-handler';
+import CommentBar from '../components/CommentBar';
+import EvilIcons from 'react-native-vector-icons/EvilIcons';
 
 type IProps = StackScreenProps<RootStackParamList, 'Detail'>;
 
 const DetailScreen = ({navigation, route}: IProps) => {
-  const {user, alt_description, description, urls, id, comments, isBookmarked} =
+  const {user, alt_description, description, urls, id, comments} =
     route.params.data;
 
-  const [commentValue, setCommentValue] = useState('');
+  console.log(comments);
+
   const [headerHeight, setHeaderHeight] = useState(0);
   const [isContentsShowing, setIsContentsShowing] = useState(true);
   const [isShowingBtn, setIsShowingBtn] = useState(false);
@@ -36,51 +30,6 @@ const DetailScreen = ({navigation, route}: IProps) => {
   const imageList = useSelector((state: IRootState) => {
     return state.imageInfo.value;
   });
-
-  const onPressSubmitCommentBtn = () => {
-    if (commentValue) {
-      setCommentValue('');
-
-      const commentData = {
-        id: new Date().valueOf(),
-        text: commentValue,
-        isLiked: false,
-      };
-
-      if (comments) {
-        const updatedList = imageList.map((imageInfo: IImageItem) =>
-          imageInfo.id === id
-            ? {
-                ...imageInfo,
-                comments: [...imageInfo.comments, commentData],
-              }
-            : imageInfo,
-        );
-        navigation.setParams({
-          data: {
-            ...route.params.data,
-            comments: [...comments, commentData],
-          },
-        });
-        dispatch(imageInfoSlice.actions.update(updatedList));
-        AsyncStorage.setItem('imageList', JSON.stringify(updatedList));
-      } else {
-        const updatedList = imageList.map((imageInfo: IImageItem) =>
-          imageInfo.id === id
-            ? {...imageInfo, comments: [commentData]}
-            : imageInfo,
-        );
-        navigation.setParams({
-          data: {
-            ...route.params.data,
-            comments: [commentData],
-          },
-        });
-        dispatch(imageInfoSlice.actions.update(updatedList));
-        AsyncStorage.setItem('imageList', JSON.stringify(updatedList));
-      }
-    }
-  };
 
   const onPressCommentLikeBtn = (commentId: number, bool: boolean) => {
     const updatedCommentList = comments.map(comment =>
@@ -103,37 +52,16 @@ const DetailScreen = ({navigation, route}: IProps) => {
     AsyncStorage.setItem('imageList', JSON.stringify(updatedImageList));
   };
 
-  const onPressBookmarkBtn = (imageId: string, bool: boolean) => {
-    const updatedList = imageList.map(el =>
-      el.id === imageId ? {...el, isBookmarked: bool} : el,
-    );
-    navigation.setParams({
-      data: {
-        ...route.params.data,
-        isBookmarked: bool,
-      },
-    });
-    dispatch(imageInfoSlice.actions.update(updatedList));
-    AsyncStorage.setItem('imageList', JSON.stringify(updatedList));
-    flatListRef.current?.scrollToOffset({offset: 608});
-  };
-
   const handleScrollEvent = e => {
     const {contentSize, layoutMeasurement, contentOffset} = e.nativeEvent;
-    if (contentOffset.y > 10) {
-      setIsShowingBtn(true);
-    } else {
-      setIsShowingBtn(false);
+    if (comments) {
+      contentOffset.y > 10 ? setIsShowingBtn(true) : setIsShowingBtn(false);
     }
 
-    if (
-      contentSize.height - layoutMeasurement.height - headerHeight <
-      contentOffset.y
-    ) {
-      setIsContentsShowing(true);
-    } else {
-      setIsContentsShowing(false);
-    }
+    contentSize.height - layoutMeasurement.height - headerHeight <
+    contentOffset.y
+      ? setIsContentsShowing(true)
+      : setIsContentsShowing(false);
   };
 
   return (
@@ -142,6 +70,14 @@ const DetailScreen = ({navigation, route}: IProps) => {
         ref={flatListRef}
         onScroll={handleScrollEvent}
         scrollEventThrottle={100}
+        ListFooterComponent={
+          comments ? null : (
+            <View style={styles.previewContentsContainer}>
+              <EvilIcons name="comment" size={50} color="#B9BDC7" />
+              <Text style={styles.previewContentsText}>생각을 나눠보세요</Text>
+            </View>
+          )
+        }
         ListHeaderComponent={
           <View
             onLayout={e => setHeaderHeight(e.nativeEvent.layout.height)}
@@ -177,56 +113,13 @@ const DetailScreen = ({navigation, route}: IProps) => {
           />
         )}
       />
-      {!isContentsShowing && (
-        <View style={styles.previewContentsContainer}>
-          <TouchableNativeFeedback
-            onPress={() => flatListRef.current?.scrollToOffset({offset: 5000})}>
-            <Text>{description}</Text>
-          </TouchableNativeFeedback>
-          <Ionicons name="chevron-up" size={20} />
-        </View>
-      )}
-      {isShowingBtn && (
-        <TouchableNativeFeedback
-          onPress={() => flatListRef.current?.scrollToEnd()}>
-          <View style={styles.moveCurrentCommentBtn}>
-            <Text style={styles.btnText}>최신 댓글로</Text>
-          </View>
-        </TouchableNativeFeedback>
-      )}
-      <View style={styles.likedIconContainer}>
-        <Ionicons
-          onPress={() => onPressBookmarkBtn(id, !isBookmarked)}
-          name="heart-sharp"
-          size={30}
-          color={isBookmarked ? '#FF7272' : '#C4C8D3'}
-          style={styles.likeBtn}
-        />
-        <Text
-          style={{
-            ...styles.likedText,
-            color: isBookmarked ? '#585A5F' : '#B9BDC7',
-          }}>
-          {isBookmarked
-            ? '좋아한 게시글이에요 !'
-            : '게시글이 맘에 들면 좋아요를 눌러보세요!'}
-        </Text>
-      </View>
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.input}
-          placeholder="생각을 나눠보세요."
-          keyboardType="numeric"
-          onChangeText={text => setCommentValue(text)}
-          clearTextOnFocus
-          value={commentValue}
-        />
-        <Ionicons
-          onPress={onPressSubmitCommentBtn}
-          name="send"
-          style={styles.sendIcon}
-        />
-      </View>
+      <CommentBar
+        data={route.params.data}
+        navigation={navigation}
+        isContentsShowing={isContentsShowing}
+        isShowingBtn={isShowingBtn}
+        flatListRef={flatListRef}
+      />
     </View>
   );
 };
@@ -274,6 +167,19 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-end',
   },
 
+  previewContentsContainer: {
+    marginTop: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  previewContentsText: {
+    fontSize: 18,
+    fontWeight: '700',
+    color: '#B9BDC7',
+    marginTop: 10,
+  },
+
   flatListContainer: {
     flexDirection: 'column-reverse',
     flexGrow: 1,
@@ -282,84 +188,5 @@ const styles = StyleSheet.create({
 
   imageContainer: {
     alignItems: 'center',
-  },
-
-  commentsContainer: {},
-
-  likedIconContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    paddingHorizontal: 15,
-    paddingVertical: 10,
-    borderTopColor: '#BCC4D8',
-    borderTopWidth: 1,
-    marginHorizontal: 10,
-    width: '100%',
-  },
-
-  likedText: {
-    fontWeight: '700',
-  },
-
-  likeBtn: {
-    marginRight: 5,
-  },
-
-  inputContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 20,
-    marginHorizontal: 10,
-  },
-
-  input: {
-    borderColor: '#BCC4D8',
-    borderRadius: 15,
-    borderWidth: 1.5,
-    padding: 12,
-    width: '100%',
-  },
-
-  sendIcon: {
-    color: '#BCC4D8',
-    fontSize: 20,
-    position: 'absolute',
-    right: 10,
-  },
-
-  previewContentsContainer: {
-    borderBottomWidth: 0.5,
-    borderColor: '#BCC4D8',
-    paddingHorizontal: 30,
-    paddingVertical: 20,
-    position: 'absolute',
-    top: 0,
-    width: '100%',
-    zIndex: 100,
-    backgroundColor: 'white',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-
-  moveCurrentCommentBtn: {
-    position: 'absolute',
-    bottom: 135,
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    borderRadius: 20,
-    backgroundColor: 'white',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 1,
-    },
-    shadowOpacity: 0.18,
-    shadowRadius: 1.0,
-    elevation: 1,
-  },
-
-  btnText: {
-    fontWeight: '900',
-    color: '#FF7272',
   },
 });
